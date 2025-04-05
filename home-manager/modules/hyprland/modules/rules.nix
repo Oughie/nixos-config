75ec024833fd2e410b92174bfaf3e2d3a01b1a82
Oughie <montgalletsabinpantin@gmail.com>
2025-04-05 15:37:02 +0200

    Update

--- home-manager/modules/hyprland/modules/rules.nix
+++ home-manager/modules/hyprland/modules/rules.nix
@@ -1,7 +1,7 @@
 {
   wayland.windowManager.hyprland.settings = {
     windowrule = [
-      "opacity 0.82 override 0.7 override 0.95 override, class:kitty"
+      "opacity 0.82 override 0.7 override 0.8 override, class:kitty"
       "opacity 0.8 override 0.7 override 0.8 override, class:discord"
     ];
   };
